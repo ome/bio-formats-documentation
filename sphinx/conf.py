61c2374a4df4caf4c8d576f35f8f7d3dee1e1560
Sebastien Besson <seb.besson@gmail.com>
2020-03-25 22:17:11 +0000

    Test ignoring biovis.com

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -435,5 +435,6 @@ linkcheck_ignore = ['https://imspector.mpibpc.mpg.de',
     r'https://testng.org/*', # Invalid SSL certificate
     r'http://www.bio-rad.com/*', # 503 Server Error with Sphinx v1.8.5  
     r'https://www.mayo.edu/.*',
-    r'https://libjpeg-turbo.org'
+    r'https://libjpeg-turbo.org',
+    r'https://www.biovis.com/.*', # SSLV3_ALERT_HANDSHAKE_FAILURE
 ]
