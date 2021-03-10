5d64f900e1657ea8af69c0cfa6d6fc301c514283
Jean-Marie Burel <j.burel@dundee.ac.uk>
2021-03-10 20:43:13 +0000

    remove extension not required

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -38,7 +38,7 @@ from datetime import datetime
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.)
-extensions = ['sphinx.ext.extlinks', 'sphinx_markdown_tables', 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.extlinks']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
