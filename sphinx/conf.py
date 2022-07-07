ee58dd380f265e47f75cce4dfee945ddda7c7047
David Gault <d.gault@dundee.ac.uk>
2022-07-07 15:16:05 +0100

    Add pro.de to linkcheck ignore list

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -449,5 +449,5 @@ linkcheck_ignore = ['https://imspector.mpibpc.mpg.de',
     r'https://valelab4.ucsf.edu/.*', # Invalid SSL certificate
     r'https://www.perkinelmer.com', # 500 server error
     r'http://www.visitech.co.uk/', # Invalid SSL certificate
-    r'http://www.rhk-tech.com', # Invalid SSL certificate
+    r'https://www.pco.de/', # Invalid SSL certificate
 ]
